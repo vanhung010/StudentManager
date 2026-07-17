25f99ba4eaae55bef5de54fd44d05b2b31997a3b
Văn Hùng <hungbuix010@gmail.com>
2026-07-17 11:18:42 +0700

    api post user

--- src/main/java/com/vhung/studentmanager/controller/UserController.java
+++ src/main/java/com/vhung/studentmanager/controller/UserController.java
@@ -2,9 +2,11 @@ package com.vhung.studentmanager.controller;
 
 import com.vhung.studentmanager.dto.request.UserRequestDTO;
 import com.vhung.studentmanager.dto.response.ApiResponse;
+import com.vhung.studentmanager.dto.response.UserResponseDTO;
 import com.vhung.studentmanager.entity.User;
 import com.vhung.studentmanager.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,7 +21,8 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping
-    public ResponseEntity<ApiResponse<User>> create(@RequestBody UserRequestDTO request){
-        return null;
+    public ResponseEntity<ApiResponse<UserResponseDTO>> create(@RequestBody UserRequestDTO request){
+        UserResponseDTO data = userService.create(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.ok(data));
     }
 }
--- src/main/java/com/vhung/studentmanager/repository/UserReposistory.java
+++ src/main/java/com/vhung/studentmanager/repository/UserReposistory.java
@@ -5,7 +5,14 @@ import org.hibernate.boot.models.JpaAnnotations;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 
 public interface UserReposistory extends JpaRepository<User, Long> {
+
+    boolean existsUserByUserName(String username);
+    //kiểm tra trùng userName khi cập nhật
+    boolean existsUserByUserNameAndIdNot(String userName, Long id);
+
 }
