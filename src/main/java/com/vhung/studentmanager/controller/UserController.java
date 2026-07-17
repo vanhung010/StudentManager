10d7689fec69da6f4530940ea22198d72b7ea863
VanHung <hungbuix010@gmail.com>
2026-07-17 11:40:17 +0700

    k

---
+++ src/main/java/com/vhung/studentmanager/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.vhung.studentmanager.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+    private final
+}
---
+++ src/main/java/com/vhung/studentmanager/service/UserService.java
@@ -0,0 +1,8 @@
+package com.vhung.studentmanager.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+
+public class UserService {
+}
